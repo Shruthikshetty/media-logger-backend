ba14d45d38f6ee855c2d0faa1933932db8d86180
shruthik shetty <shrutikshetty14@gmail.com>
2025-07-20 20:02:01 +0530

    add endpoint to bulk delete  movies

--- src/routes/movie.route.ts
+++ src/routes/movie.route.ts
@@ -9,11 +9,13 @@ import {
   getAllMovies,
   getMovieById,
   updateMovieById,
+  bulkDeleteMovies,
 } from '../controllers/movie.controller';
 import { requireAuth } from '../common/middleware/require-auth';
 import { validateReq } from '../common/middleware/handle-validation';
 import { AddMovieZodSchema } from '../common/validation-schema/movie/add-movie';
 import { updateMoveZodSchema } from '../common/validation-schema/movie/update-movie';
+import { BulkDeleteMovieZodSchema } from '../common/validation-schema/movie/bulk-delete';
 
 // initialize router
 const route = Router();
@@ -26,6 +28,14 @@ route.get('/:id', getMovieById);
 
 // add a movie
 route.post('/', requireAuth('admin'), validateReq(AddMovieZodSchema), addMovie);
+
+//Route to bulk delete movies
+route.delete(
+  '/bulk',
+  requireAuth('admin'),
+  validateReq(BulkDeleteMovieZodSchema),
+  bulkDeleteMovies
+);
 
 // delete movie by id
 route.delete('/:id', requireAuth('admin'), deleteMovieById);
