52c108c8d0c23ce4e678bb072c3e51121db18ee1
shruthik shetty <shrutikshetty14@gmail.com>
2025-08-02 13:25:44 +0530

    update e2e test

---
+++ src/__mocks__/mock-users.ts
@@ -0,0 +1,14 @@
+/**
+ * @file contains data mocks required for e2e testing for user endpoints
+ */
+
+export const mockTestUsers = [
+  { name: 'Alice', email: 'alice@example.com', password: 'password123' },
+  { name: 'Bob', email: 'bob@example.com', password: 'password123' },
+  {
+    name: 'Admin',
+    email: 'Admin@example.com',
+    password: 'password123',
+    role: 'admin',
+  },
+];
