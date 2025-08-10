87bbaa03f7d3c6c3f524f704ae2001b971268b7c
shruthik shetty <shrutikshetty14@gmail.com>
2025-08-10 21:13:35 +0530

    add e2e test for bulk delete games

--- src/__e2e__/games-delete.test.ts
+++ src/__e2e__/games-delete.test.ts
@@ -72,7 +72,7 @@ describe('all the games delete related endpoints', () => {
       expect(res.status).toBe(404);
       expect(res.body.success).toBe(false);
       expect(res.body.message).toMatch(/Game does not exist/);
-    })
+    });
 
     it('should return 400 if invalid game id', async () => {
       // login user
@@ -88,7 +88,7 @@ describe('all the games delete related endpoints', () => {
       expect(res.status).toBe(400);
       expect(res.body.success).toBe(false);
       expect(res.body.message).toMatch(/Invalid game id/);
-    })
+    });
 
     it('should return 401 for unauthenticated user', async () => {
       // delete the created id
@@ -97,6 +97,68 @@ describe('all the games delete related endpoints', () => {
       expect(res.status).toBe(401);
       expect(res.body.success).toBe(false);
       expect(res.body.message).toMatch(/Unauthorized/);
-    })
+    });
+  });
+
+  describe('delete bulk games DELETE /api/game/bulk', () => {
+    it('should delete bulk games with 200', async () => {
+      // login user
+      await loginUser();
+
+      // delete the created id
+      const res = await supertest(app)
+        .delete(`/api/game/bulk`)
+        .set({
+          Authorization: `Bearer ${token}`,
+        })
+        .send({ gameIds: [gameId1] });
+      // assertions
+      expect(res.status).toBe(200);
+      expect(res.body.success).toBe(true);
+      expect(res.body.message).toMatch(/deleted successfully/i);
+      expect(res.body.data).toEqual(expect.objectContaining({ deleCount: 1 }));
+    });
+
+    it('should return 401 for unauthenticated user', async () => {
+      // delete the created id
+      const res = await supertest(app).delete(`/api/game/bulk`);
+      // assertions
+      expect(res.status).toBe(401);
+      expect(res.body.success).toBe(false);
+      expect(res.body.message).toMatch(/Unauthorized/);
+    });
+
+    it('should return 400 if invalid game id', async () => {
+      // login user
+      await loginUser();
+
+      // delete the created id
+      const res = await supertest(app)
+        .delete(`/api/game/bulk`)
+        .set({
+          Authorization: `Bearer ${token}`,
+        })
+        .send({ gameIds: ['invalid-id'] });
+      // assertions
+      expect(res.status).toBe(400);
+      expect(res.body.success).toBe(false);
+      expect(res.body.message).toMatch(/Invalid game id/);
+    });
+    it('should return 404 if game not found', async () => {
+      // login user
+      await loginUser();
+
+      // delete the created id
+      const res = await supertest(app)
+        .delete(`/api/game/bulk`)
+        .set({
+          Authorization: `Bearer ${token}`,
+        })
+        .send({ gameIds: [new mongoose.Types.ObjectId()] });
+      // assertions
+      expect(res.status).toBe(404);
+      expect(res.body.success).toBe(false);
+      expect(res.body.message).toMatch(/No games found/);
+    });
   });
 });
