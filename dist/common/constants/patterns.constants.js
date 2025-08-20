22d7897222ec77ac25a3ab0d3e1ebf40ab3e6d28
shruthik shetty <shrutikshetty14@gmail.com>
2025-08-20 12:33:22 +0530

    comment fix

--- dist/common/constants/patterns.constants.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-/**
- * Contains all the regular expression patterns
- */
-Object.defineProperty(exports, '__esModule', { value: true });
-exports.Regex = void 0;
-exports.Regex = {
-  email: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-};
