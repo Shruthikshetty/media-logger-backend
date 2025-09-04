9f4eb7e39d0a9463562fa859853eda5cd563a845
shruthik shetty <shrutikshetty14@gmail.com>
2025-09-04 21:18:08 +0530

    update movies filter endpoint

--- src/common/validation-schema/movie/movie-filters.ts
+++ src/common/validation-schema/movie/movie-filters.ts
@@ -4,144 +4,134 @@
 
 import z from 'zod';
 import { GET_ALL_MOVIES_LIMITS } from '../../constants/config.constants';
-import { omit } from 'lodash';
 
-export const MovieFiltersZodSchema = z
-  .object({
-    averageRating: z
-      .number({
-        message: 'Average rating must be number',
-      })
-      .optional(),
+export const MovieFiltersZodSchema = z.object({
+  averageRating: z
+    .number({
+      message: 'Average rating must be number',
+    })
+    .optional(),
 
-    genre: z
-      .array(z.string({ message: 'Genre must be string' }), {
-        message: 'Genre must be an array of strings',
-      })
-      .optional(),
+  genre: z
+    .array(z.string({ message: 'Genre must be string' }), {
+      message: 'Genre must be an array of strings',
+    })
+    .optional(),
 
-    releaseDate: z
-      .object({
-        lte: z
-          .string({
-            message: 'Release date lte must be string',
-          })
-          .datetime({
-            message:
-              'Release date lte must be in ISO 8601 format (YYYY-MM-DDTHH:mm:ss.sssZ)',
-          })
-          .transform((val) => new Date(val))
-          .optional(),
-        gte: z
-          .string({
-            message: 'Release date gte must be string',
-          })
-          .datetime({
-            message:
-              'Release date gte must be in ISO 8601 format (YYYY-MM-DDTHH:mm:ss.sssZ)',
-          })
-          .transform((val) => new Date(val))
-          .optional(),
-      })
-      .refine((data) => data.gte !== undefined || data.lte !== undefined, {
-        message: 'Release date must include at least one of gte or lte',
-      })
-      .optional(),
+  releaseDate: z
+    .object({
+      lte: z
+        .string({
+          message: 'Release date lte must be string',
+        })
+        .datetime({
+          message:
+            'Release date lte must be in ISO 8601 format (YYYY-MM-DDTHH:mm:ss.sssZ)',
+        })
+        .transform((val) => new Date(val))
+        .optional(),
+      gte: z
+        .string({
+          message: 'Release date gte must be string',
+        })
+        .datetime({
+          message:
+            'Release date gte must be in ISO 8601 format (YYYY-MM-DDTHH:mm:ss.sssZ)',
+        })
+        .transform((val) => new Date(val))
+        .optional(),
+    })
+    .refine((data) => data.gte !== undefined || data.lte !== undefined, {
+      message: 'Release date must include at least one of gte or lte',
+    })
+    .optional(),
 
-    runTime: z
-      .object({
-        gte: z
-          .number({
-            message: 'Run time gte must be number',
-          })
-          .min(0)
-          .optional(),
-        lte: z
-          .number({
-            message: 'Run time lte must be number',
-          })
-          .min(0)
-          .optional(),
-      })
-      .refine((data) => data.gte !== undefined || data.lte !== undefined, {
-        message: 'Run time must include at least one of gte or lte',
-      })
-      .optional(),
+  runTime: z
+    .object({
+      gte: z
+        .number({
+          message: 'Run time gte must be number',
+        })
+        .min(0)
+        .optional(),
+      lte: z
+        .number({
+          message: 'Run time lte must be number',
+        })
+        .min(0)
+        .optional(),
+    })
+    .refine((data) => data.gte !== undefined || data.lte !== undefined, {
+      message: 'Run time must include at least one of gte or lte',
+    })
+    .optional(),
 
-    languages: z
-      .array(
-        z
-          .string({
-            message: 'Languages must be string',
-          })
-          .transform((val) => val.toLowerCase())
-      )
-      .refine((data) => data.length > 0, {
-        message:
-          'Languages must be an array of strings with at least one value',
-      })
-      .optional(),
+  languages: z
+    .array(
+      z
+        .string({
+          message: 'Languages must be string',
+        })
+        .transform((val) => val.toLowerCase())
+    )
+    .refine((data) => data.length > 0, {
+      message: 'Languages must be an array of strings with at least one value',
+    })
+    .optional(),
 
-    tags: z
-      .array(z.string({ message: 'Tags must be string' }), {
-        message: 'Tags must be an array of strings',
-      })
-      .optional(),
+  tags: z
+    .array(z.string({ message: 'Tags must be string' }), {
+      message: 'Tags must be an array of strings',
+    })
+    .optional(),
 
-    status: z
-      .string({
-        required_error: 'Status is required',
-        message: 'Status must be string',
-      })
-      .optional(),
+  status: z
+    .string({
+      required_error: 'Status is required',
+      message: 'Status must be string',
+    })
+    .optional(),
 
-    ageRating: z
-      .object({
-        gte: z
-          .number({
-            message: 'Age rating gte must be number',
-          })
-          .min(0)
-          .optional(),
-        lte: z
-          .number({
-            message: 'Age rating lte must be number',
-          })
-          .min(0)
-          .optional(),
-      })
-      .refine((data) => data.gte !== undefined || data.lte !== undefined, {
-        message: 'Age rating must include at least one of gte or lte',
-      })
-      .optional(),
+  ageRating: z
+    .object({
+      gte: z
+        .number({
+          message: 'Age rating gte must be number',
+        })
+        .min(0)
+        .optional(),
+      lte: z
+        .number({
+          message: 'Age rating lte must be number',
+        })
+        .min(0)
+        .optional(),
+    })
+    .refine((data) => data.gte !== undefined || data.lte !== undefined, {
+      message: 'Age rating must include at least one of gte or lte',
+    })
+    .optional(),
 
-    limit: z
-      .number({
-        message: 'Limit must be number',
-      })
-      .max(GET_ALL_MOVIES_LIMITS.limit.max)
-      .min(GET_ALL_MOVIES_LIMITS.limit.min)
-      .default(GET_ALL_MOVIES_LIMITS.limit.default),
+  limit: z
+    .number({
+      message: 'Limit must be number',
+    })
+    .max(GET_ALL_MOVIES_LIMITS.limit.max)
+    .min(GET_ALL_MOVIES_LIMITS.limit.min)
+    .default(GET_ALL_MOVIES_LIMITS.limit.default),
 
-    page: z
-      .number({
-        message: 'Page must be number',
-      })
-      .min(1)
-      .default(1),
-  })
-  .superRefine((data, ctx) => {
-    const hasValue = Object.values(omit(data, 'limit', 'start', 'page')).some(
-      (val) => val !== undefined
-    );
-    if (!hasValue) {
-      ctx.addIssue({
-        code: z.ZodIssueCode.custom,
-        message: 'Provide filter for results to appear',
-        path: [],
-      });
-    }
-  });
+  page: z
+    .number({
+      message: 'Page must be number',
+    })
+    .min(1)
+    .default(1),
+  searchText: z
+    .string({
+      message: 'Search text must be string',
+    })
+    .optional(),
+});
 
 //export type
 export type MovieFiltersZodType = z.infer<typeof MovieFiltersZodSchema>;
