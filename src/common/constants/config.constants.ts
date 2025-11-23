3551d6991dd7b2bfba61074ce1560f7d25c8112d
shruthik shetty <shrutikshetty14@gmail.com>
2025-11-23 13:31:10 +0530

    set up recommender service and add health endpoint .

--- src/common/constants/config.constants.ts
+++ src/common/constants/config.constants.ts
@@ -93,3 +93,5 @@ export const HISTORY_RETENTION_DAYS = 180;
 
 //sanitization custom rules
 export const CUSTOM_SANITIZATION_RULES = ['password', 'token'];
+
+export const RECOMMENDER_MS_HEALTH_CHECK_TIMEOUT = 2000;
