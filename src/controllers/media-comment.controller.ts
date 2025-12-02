c7e25b78eb30dd9b98a15ab49a76b30a8ab85af4
shruthik shetty <shrutikshetty14@gmail.com>
2025-12-02 19:42:14 +0530

    convert convex services to package

--- src/controllers/media-comment.controller.ts
+++ src/controllers/media-comment.controller.ts
@@ -6,7 +6,7 @@ import { handleError } from '../common/utils/handle-error';
 import { ValidatedRequest } from '../types/custom-types';
 import { Response } from 'express';
 import { ConvexHttpClient } from 'convex/browser';
-import { api } from '../convex/_generated/api';
+import { api } from 'media-logger-convex-api-services';
 import { AddMediaCommentSchemaType } from '../common/validation-schema/media-comment/add-comment';
 import { isMongoIdValid } from '../common/utils/mongo-errors';
 import { HISTORY_ENTITY } from '../common/constants/model.constants';
@@ -24,7 +24,7 @@ export const addMediaComment = async (
     const mockComment = {
       entityId: validatedData.entityId,
       entityType: validatedData.entityType,
-      user: req.userData?._id as string,
+      user: req.userData?._id.toString(),
       comment: validatedData.comment,
       username: req.userData?.name,
       profileImg: req.userData?.profileImg,
