5388d426b5ec343acb348f071b127048627af366
shruthik shetty <shrutikshetty14@gmail.com>
2025-12-02 20:06:44 +0530

    change

--- src/common/config/convex.config.ts
+++ src/common/config/convex.config.ts
@@ -1,9 +1,4 @@
 import { ConvexHttpClient } from 'convex/browser';
 
-// create convex client
-if (!process.env.CONVEX_URL) {
-  throw new Error('CONVEX_URL environment variable is required');
-}
-
-const convex = new ConvexHttpClient(process.env.CONVEX_URL);
+const convex = new ConvexHttpClient(process.env.CONVEX_URL ?? '');
 export default convex;
