1694aa4db8734f8bc0c39e3d6274903893c9e2ad
shruthik shetty <shrutikshetty14@gmail.com>
2025-12-04 20:46:36 +0530

    publish new convex function package

--- packages/convex-api/src/convexApi.ts
+++ packages/convex-api/src/convexApi.ts
@@ -1,5 +1,5 @@
-import { type FunctionReference, anyApi } from 'convex/server';
-import { type GenericId as Id } from 'convex/values';
+import { type FunctionReference, anyApi } from "convex/server";
+import { type GenericId as Id } from "convex/values";
 
 export const api: PublicApiType = anyApi as unknown as PublicApiType;
 export const internal: InternalApiType = anyApi as unknown as InternalApiType;
@@ -8,8 +8,8 @@ export type PublicApiType = {
   services: {
     comments: {
       createMediaCommentMutation: FunctionReference<
-        'mutation',
-        'public',
+        "mutation",
+        "public",
         {
           comment: string;
           entityId: string;
@@ -20,15 +20,33 @@ export type PublicApiType = {
         },
         any
       >;
+      deleteMediaCommentById: FunctionReference<
+        "mutation",
+        "public",
+        { id: Id<"mediaComments"> },
+        any
+      >;
+      getMediaById: FunctionReference<
+        "query",
+        "public",
+        { id: Id<"mediaComments"> },
+        any
+      >;
       getMediaCommentsQuery: FunctionReference<
-        'query',
-        'public',
+        "query",
+        "public",
         {
           cursor?: string | null;
           entityId: string;
           entityType: string;
           limit?: number;
         },
+        any
+      >;
+      updateMediaCommentById: FunctionReference<
+        "mutation",
+        "public",
+        { comment: string; id: Id<"mediaComments"> },
         any
       >;
     };
