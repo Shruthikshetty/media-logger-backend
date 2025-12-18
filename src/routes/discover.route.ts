012328bb998882c8c9fc46b9c9d7737a0c25a3bd
shruthik shetty <shrutikshetty14@gmail.com>
2025-12-18 20:21:19 +0530

    add swagger for discover endpoints

--- src/routes/discover.route.ts
+++ src/routes/discover.route.ts
@@ -13,13 +13,91 @@ import { optionalAuth } from '../common/middleware/require-auth';
 //initialize router
 const route = Router();
 
-// discover games with user entries mapped for games
+/**
+ * @swagger
+ * /api/discover/games:
+ *   get:
+ *     tags: [Discovery]
+ *     summary: Fetch games with populated user states .
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: query
+ *         name: limit
+ *         require: false
+ *         type: integer
+ *         default: 20
+ *       - name: page
+ *         in: query
+ *         require: false
+ *         type: integer
+ *         default: 20
+ *     responses:
+ *       '200':
+ *         $ref: '#/components/responses/GetDiscoverGamesSuccessResponse'
+ *       '401':
+ *         $ref: '#/components/responses/Unauthorized'
+ *       '500':
+ *         $ref: '#/components/responses/InternalServerError'
+ */
 route.get('/games', optionalAuth(), getDiscoverGames);
 
-// discover movies with user entries mapped for movies
+/**
+ * @swagger
+ * /api/discover/movies:
+ *   get:
+ *     tags: [Discovery]
+ *     summary: Fetch movies with populated user states .
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: query
+ *         name: limit
+ *         require: false
+ *         type: integer
+ *         default: 20
+ *       - name: page
+ *         in: query
+ *         require: false
+ *         type: integer
+ *         default: 20
+ *     responses:
+ *       '200':
+ *         $ref: '#/components/responses/GetDiscoverMoviesSuccessResponse'
+ *       '401':
+ *         $ref: '#/components/responses/Unauthorized'
+ *       '500':
+ *         $ref: '#/components/responses/InternalServerError'
+ */
 route.get('/movies', optionalAuth(), getDiscoverMovies);
 
-// discover tv series with user entries mapped for tv series
+/**
+ * @swagger
+ * /api/discover/tv-show:
+ *   get:
+ *     tags: [Discovery]
+ *     summary: Fetch tv shows with populated user states .
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: query
+ *         name: limit
+ *         require: false
+ *         type: integer
+ *         default: 20
+ *       - name: page
+ *         in: query
+ *         require: false
+ *         type: integer
+ *         default: 20
+ *     responses:
+ *       '200':
+ *         $ref: '#/components/responses/GetDiscoverTvShowSuccessResponse'
+ *       '401':
+ *         $ref: '#/components/responses/Unauthorized'
+ *       '500':
+ *         $ref: '#/components/responses/InternalServerError'
+ */
 route.get('/tv-show', optionalAuth(), getDiscoverTVSeries);
 
 //export all routes
